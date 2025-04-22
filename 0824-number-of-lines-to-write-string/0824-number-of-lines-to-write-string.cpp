831b754bd18084b6b4f9d6684e8a78d126dff04a
Anshul Jadhao <148558665+jadhaoanshul@users.noreply.github.com>
2025-04-23 00:18:04 +0530

    Time: 0 ms (100%), Space: 9.2 MB (62.6%) - LeetHub

--- 0824-number-of-lines-to-write-string/0824-number-of-lines-to-write-string.cpp
+++ 0824-number-of-lines-to-write-string/0824-number-of-lines-to-write-string.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     vector<int> numberOfLines(vector<int>& widths, string s) {
-        int maxWidth = 100;
-        int lines = 1; // Start with one line
-        int currentWidth = 0;
-        
-        for (char c : s) {
-            int width = widths[c - 'a']; // Map 'a'-'z' to 0-25
-            if (currentWidth + width > maxWidth) {
-                lines++;
-                currentWidth = width;
+        int val = 0, c = 1;  // Start with line 1
+        for (int i = 0; i < s.size(); i++) {
+            int w = widths[s[i] - 'a'];
+            if (val + w > 100) {
+                c++;        // move to next line
+                val = w;    // start new line with current character
             } else {
-                currentWidth += width;
+                val += w;
             }
         }
-
-        return {lines, currentWidth};
+        return {c, val};
     }
 };
